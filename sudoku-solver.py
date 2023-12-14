5b7b0d1c689b91c9823e0a1a6d7b535ce1d698f4
Hang020713 <hang020713@gmail.com>
2023-12-14 09:32:47 +0800

    creating new solver
    v2

--- sudoku-solver.py
+++ sudoku-solver.py
@@ -1,7 +1,217 @@
-def scanning(data):
+def get_grid_number(r, c):
+    return int(r/3) * 3 + int(c/3)
+
+def create_empty_map():
+    map = []
+    for r in range(9):
+        map.append([0] * 9)
+
+    return map
+
+def print_map(map):
+    row = 0
+    col = 0
+    for r in range(9):
+        row %= 3
+        if(row == 0):
+            print("-"*25)
+
+        for c in range(9):
+            col %= 3
+            if(col == 0):
+                print("| ", end="")
+
+            print(map[r][c], "", end="")
+            col += 1
+
+        print("|")
+        row += 1
+    print("-"*25)
+
+def scanning(map, action_log):
+    #Make a map check
+    for n in range(1, 10):
+        check_map = create_empty_map()
+        #print("[Scanning] Now checking", n)
+
+        for r in range(9):
+            for c in range(9):
+                #Have other number
+                if(not map[r][c] == 0):
+                    check_map[r][c] = 1
+
+                #Same Number
+                if(map[r][c] == n):
+                    #Found same, put it into check map
+                    #Fill row & col
+                    for f in range(9):
+                        check_map[r][f] = 1
+                        check_map[f][c] = 1
+
+                    #Fill grid
+                    gr = int(get_grid_number(r, c) / 3) * 3
+                    gc = int(get_grid_number(r, c) % 3) * 3
+                    for cr in range(3):
+                        for cc in range(3):
+                            check_map[gr+cr][gc+cc] = 1
+        
+        #Check is there only 1 empty in grid
+        '''
+        Sample
+        1 1 1
+        1 1 1
+        1 0 1
+        '''
+        '''
+        0-2, 3-5, 6-8
+        '''
+        #g for grid top-left position
+        #suppose to be 0, 3, 6
+        for gr in range(0, 7, 3):
+            for gc in range(0, 7, 3):
+                last_empty_pos = { "x":-1, "y":-1 }
+                empty_count = 0
+                for cr in range(3):
+                    for cc in range(3):
+                        if(check_map[gr+cr][gc+cc] == 1):
+                            empty_count += 1
+                        else:
+                            last_empty_pos['x'] = gc+cc
+                            last_empty_pos['y'] = gr+cr
+                if(empty_count == 8):
+                    '''
+                    print("[Scanning] Found one,",
+                          "num:", n,
+                          ", grid:", get_grid_number(gr, gc), 
+                          ", r:", last_empty_pos['y'],
+                          ", c:", last_empty_pos['x'])
+                    '''
+                    action_log.append({
+                        "action": "scanning",
+                        "number": n,
+                        "grid": get_grid_number(gr, gc),
+                        "row": last_empty_pos['y'],
+                        "col": last_empty_pos['x']
+                    })
+
+                    #Fill
+                    #print('[Scanning] Before:')
+                    #print_map(map)
+                    #print('[Scanning] After:')
+                    map[last_empty_pos['y']][last_empty_pos['x']] = n
+                    #print_map(map)
+                    return True
+
     return False
 
-#data[y][x]
-data = [
-    []
+def single_candidate(map, action_log):
+    #Search square by square
+    for r in range(9):
+        for c in range(9):
+            #Already have number
+            if(not map[r][c] == 0):
+                continue
+
+            #Check number
+            check_number = 0
+            check_number_list = [False] * 9
+
+            #Search col & row
+            for p in range(9):
+                if(not map[r][p] == 0):
+                    if(not check_number_list[map[r][p] - 1]):
+                        check_number += 1
+                        check_number_list[map[r][p] - 1] = True
+
+                if(not map[p][c] == 0):
+                    if(not check_number_list[map[p][c] - 1]):
+                        check_number += 1
+                        check_number_list[map[p][c] - 1] = True
+
+            #Search grid
+            gr = int(get_grid_number(r, c) / 3) * 3
+            gc = int(get_grid_number(r, c) % 3) * 3
+            for cr in range(3):
+                for cc in range(3):
+                    #print(gr + cr, gc + cc)
+                    if(not map[gr + cr][gc + cc] == 0):
+                        if(not check_number_list[map[gr + cr][gc + cc] - 1]):
+                            check_number += 1
+                            check_number_list[map[gr + cr][gc + cc] - 1] = True
+
+            #Check last
+            if(check_number == 8):
+                last_num = 1
+                for p in check_number_list:
+                    if(not p):
+                       break
+                    
+                    last_num += 1
+
+                map[r][c] = last_num
+
+                '''
+                print("[Single Candidate] Found",
+                      ", number:", last_num,
+                      ", grid:", get_grid_number(r, c),
+                      ", r:", r,
+                      ", c:", c)
+                '''
+                
+                action_log.append({
+                    "action": "single_candidate",
+                    "number": last_num,
+                    "row": r,
+                    "col": c
+                })
+                return True
+
+    return False
+
+def eliminating_number(map, action_log):
+    #Make a check map pin all number
+    ori_check_map = create_empty_map()
+    for r in range(9):
+        for c in range(9):
+            if(not map[r][c] == 0):
+                ori_check_map[r][c] = 1
+    
+    print_map(ori_check_map)
+
+    return False
+
+#sudoku data[y][x]
+#0 for empty
+map = [
+    [0, 0, 0, 1, 0, 4, 0, 0, 0],
+    [0, 0, 1, 0, 0, 0, 9, 0, 0],
+    [0, 9, 0, 7, 0, 3, 0, 6, 0],
+    [8, 0, 7, 0, 0, 0, 1, 0, 6],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [3, 0, 4, 0, 0, 0, 5, 0, 9],
+    [0, 5, 0, 4, 0, 2, 0, 3, 0],
+    [0, 0, 8, 0, 0, 0, 6, 0, 0],
+    [0, 0, 0, 8, 0, 6, 0, 0, 0]
 ]
+#action, number, row, col
+action_log = []
+
+print("Original:")
+print_map(map)
+
+#Start checking
+eliminating_number(map, action_log)
+'''
+while True:
+    if(scanning(map, action_log)): continue
+    if(single_candidate(map, action_log)): continue
+
+    break
+'''
+
+print('End, no skill can be applied')
+for action in action_log:
+    print(action['action'], ", number:", action['number'], ', row:', action['row'], ', col:', action['col'])
+
+print('End')
+print_map(map)
---
+++ sudoku-solver2.py
@@ -0,0 +1,392 @@
+import time
+'''
+unit: box, row, col, grid
+Index: start from 0
+Box: 0 1 2
+     3 4 5
+     6 7 8
+sudoku[y][x]
+
+last remaining cell: check number
+
+last free cell: check empty
+last possible number: check empty
+obvious singles: check empty
+obvious pairs: check empty & remove other have same numbers
+    condition: have two same combination in same row/col/grid
+obvious triples:
+    condition:
+    (123) (123) (123) - {3/3/3} (in terms of candidates per cell)
+    (123) (123) (12) - {3/3/2} (or some combination thereof)
+    (123) (12) (23) - {3/2/2}
+    (12) (23) (13) - {2/2/2}
+hidden quads: ---
+hidden singles: check empty, check for one and only one
+pointing paris: check empty, remove same numbers on grid
+'''
+
+'''Helper functions'''
+def create_empty_sudoku():
+    sudoku = []
+    for r in range(9):
+        sudoku.append([0] * 9)
+
+    return sudoku
+
+def print_sudoku(sudoku):
+    row = 0
+    col = 0
+    for r in range(9):
+        row %= 3
+        if(row == 0):
+            print("-"*25)
+
+        for c in range(9):
+            col %= 3
+            if(col == 0):
+                print("| ", end="")
+
+            print(sudoku[r][c], "", end="")
+            col += 1
+
+        print("|")
+        row += 1
+    print("-"*25)
+
+def getBoxXYFromBoxNum(box):    
+    x = box % 3 * 3
+    y = int(box / 3) * 3
+    return [x, y]
+
+def getBoxNumFromXY(x, y):
+    return int(y / 3) * 3 + int(x / 3)
+
+def getBoxXYFromXY(x, y):
+    return getBoxXYFromBoxNum(getBoxNumFromXY(x, y))
+
+def getBoxRemainingNumber(box):
+    box = getBoxXYFromBoxNum(box)
+    checking = [item for item in range(1, 10)]
+
+    for r in range(box[1], box[1] + 3):
+        for c in range(box[0], box[0] + 3):
+            if(modified_sudoku[r][c] != 0):
+                checking.remove(modified_sudoku[r][c])
+
+def countElement(list, element):
+    count = 0
+    for i in range(len(list)):
+        if(list[i] == element):
+            count += 1
+    
+    return count
+
+#This function will loop number by number
+#Check filled sudoku is there one remaining grid
+#0 for empty, 1 for filled
+def last_remaining_cell():
+    for n in range(1, 10):
+        #A empty sudoku for storing filled grid
+        checking = create_empty_sudoku()
+
+        #Start filling
+        for r in range(9):
+            for c in range(9):
+                val = modified_sudoku[r][c]
+                if(val == n):
+                    #Fill box
+                    box = getBoxXYFromXY(c, r)
+                    for gr in range(3):
+                        for gc in range(3):
+                            checking[box[1] + gr][box[0] + gc] = 1
+                    
+                    #Fill row
+                    for rr in range(9):
+                        checking[rr][c] = 1
+
+                    #Fill col
+                    for cc in range(9):
+                        checking[r][cc] = 1
+                elif(val != 0):
+                    checking[r][c] = 1
+
+        #Check remaining one grid
+        #Box
+        for b in range(9):
+            count = 0
+            box = getBoxXYFromBoxNum(b)
+            last_empty_grid = [-1, -1]
+            #Find 0 amount
+            for r in range(box[1], box[1] + 3):
+                for c in range(box[0], box[0] + 3):
+                    if(checking[r][c] == 0):
+                        count += 1
+                        last_empty_grid[0] = c
+                        last_empty_grid[1] = r
+
+            #If amount == 1, then fill it
+            if(count == 1):
+                modified_sudoku[last_empty_grid[1]][last_empty_grid[0]] = n
+                action_log.append([
+                    "last_remaining_cell_box",
+                    last_empty_grid[0],
+                    last_empty_grid[1],
+                    n
+                ])
+                getNotes()
+                return True
+        
+        #Row
+        for r in range(9):
+            count = 0
+            last_empty_grid = [-1, -1]
+            #Find 0 amount
+            for c in range(9):
+                if(checking[r][c] == 0):
+                    count += 1
+                    last_empty_grid[0] = c
+                    last_empty_grid[1] = r
+
+            #If amount == 1, then fill it
+            if(count == 1):
+                modified_sudoku[last_empty_grid[1]][last_empty_grid[0]] = n
+                action_log.append([
+                    "last_remaining_cell_row",
+                    last_empty_grid[0],
+                    last_empty_grid[1],
+                    n
+                ])
+                getNotes()
+                return True
+        
+        #Col
+        for c in range(9):
+            count = 0
+            last_empty_grid = [-1, -1]
+            #Find 0 amount
+            for r in range(9):
+                if(checking[r][c] == 0):
+                    count += 1
+                    last_empty_grid[0] = c
+                    last_empty_grid[1] = r
+
+            #If amount == 1, then fill it
+            if(count == 1):
+                modified_sudoku[last_empty_grid[1]][last_empty_grid[0]] = n
+                action_log.append([
+                    "last_remaining_cell_row",
+                    last_empty_grid[0],
+                    last_empty_grid[1],
+                    n
+                ])
+                getNotes()
+                return True
+    
+    return False
+
+#0 for possible, 1 for filled
+def getNote(x, y):
+    if(modified_sudoku[y][x] != 0):
+        return []
+
+    #Index - 1
+    possible = [0] * 9
+
+    #Loop Box
+    box = getBoxXYFromXY(x, y)
+    for gr in range(box[1], box[1] + 3):
+        for gc in range(box[0], box[0] + 3):
+            val = modified_sudoku[gr][gc]
+            if(val != 0):
+                possible[val - 1] = 1
+                
+    #Loop Row
+    for gr in range(9):
+        val = modified_sudoku[gr][x]
+        if(val != 0):
+            possible[val - 1] = 1
+
+    #Loop Col
+    for gc in range(9):
+        val = modified_sudoku[y][gc]
+        if(val != 0):
+            possible[val - 1] = 1
+
+    return possible
+
+def create_empty_sudoku_note():
+    sudoku_note.clear()
+    for r in range(9):
+        sudoku_note.append([] * 9)
+        for c in range(9):
+            sudoku_note[r].append([] * 9)
+
+    return sudoku_note
+
+def getNotes():
+    create_empty_sudoku_note()
+
+    #Loop through grid and get all notes
+    for r in range(9):
+        for c in range(9):
+            sudoku_note[r][c] = getNote(c, r)
+
+def getNoteNumbers(sets):
+    numbers = []
+    for i in range(len(sets)):
+        if(sets[i] == 0):
+            numbers.append(i+1)
+    return numbers
+
+def print_notes():
+    for r in range(9):
+        print("-"*10)
+        for c in range(9):
+            numbers = getNoteNumbers(sudoku_note[r][c])
+            print("X:", c, ", Y:", r, ":", numbers)
+
+def obvious():
+    #Loop through grid and get all notes
+    for r in range(9):
+        for c in range(9):
+            #obvious singles
+            if(countElement(sudoku_note[r][c], 0) == 1):
+                modified_sudoku[r][c] = sudoku_note[r][c].index(0) + 1
+                action_log.append([
+                    "obvious_singles",
+                    c,
+                    r,
+                    sudoku_note[r][c].index(0) + 1
+                ])
+                #getNotes()  #Reset notes
+                return False
+
+    #Pairs or Triples
+    '''
+    obvious pairs: check empty & remove other have same numbers
+    condition: have two same combination in same row/col/grid
+        obvious triples:
+        condition:
+        (123) (123) (123) - {3/3/3} (in terms of candidates per cell)
+        (123) (123) (12) - {3/3/2} (or some combination thereof)
+        (123) (12) (23) - {3/2/2}
+        (12) (23) (13) - {2/2/2}
+    '''
+    for r in range(9):
+        for c in range(9):
+            #Pairs
+            if(countElement(sudoku_note[r][c], 0) == 2):
+                same_notes = 0
+                same_notes_grid = [
+                    [c, r]
+                ]
+                pairs_number = getNoteNumbers(sudoku_note[r][c])
+
+                #Check have other grid, row, col notes have same numbers
+                #Box
+                box = getBoxXYFromXY(c, r)
+                for gr in range(box[1], box[1] + 3):
+                    for gc in range(box[0], box[0] + 3):
+                        if(sudoku_note[r][c] == sudoku_note[gr][gc] and gr != r and gc != c):
+                            same_notes += 1
+                            same_notes_grid.append([gc, gr])
+
+                #Row
+                for gr in range(9):
+                    if(sudoku_note[r][c] == sudoku_note[gr][c] and gr != r):
+                        same_notes += 1
+                        same_notes_grid.append([c, gr])
+                
+                #Col
+                for gc in range(9):
+                    if(sudoku_note[r][c] == sudoku_note[r][gc] and gc != c):
+                        same_notes += 1
+                        same_notes_grid.append([gc, r])
+
+                #If yes, then remove same unit other notes
+                if(same_notes != 0):
+                    #Add records
+                    action_log.append([
+                        "obvious_pairs",
+                        same_notes_grid,
+                        pairs_number
+                    ])
+
+                    #Remove
+                    #Box
+                    for gr in range(box[1], box[1] + 3):
+                        for gc in range(box[0], box[0] + 3):
+                            if(countElement(sudoku_note[gr][gc], 0) > 2):
+                                for i in range(2):
+                                    sudoku_note[gr][gc][pairs_number[i] - 1] = 1
+
+                    #Let's go again
+                    return True  #End
+
+    return False
+
+#Main Program
+#Skill, x, y, number
+action_log = []
+'''
+original_sudoku = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0]
+]
+'''
+original_sudoku = [
+    [0, 0, 2, 0, 8, 5, 0, 0, 4],
+    [0, 0, 0, 0, 3, 0, 0, 6, 0],
+    [0, 0, 4, 2, 1, 0, 0, 3, 0],
+    [0, 0, 0, 0, 0, 0, 0, 5, 2],
+    [0, 0, 0, 0, 0, 0, 0, 1, 0],
+    [9, 0, 0, 0, 0, 0, 0, 0, 0],
+    [8, 0, 0, 0, 0, 6, 0, 0, 0],
+    [2, 5, 0, 4, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 1, 6, 0, 0]
+]
+modified_sudoku = original_sudoku
+sudoku_note = []
+
+'''
+getNotes()
+obvious()
+print_sudoku(modified_sudoku)
+print(action_log) 
+'''
+
+def debug():
+    print_notes()
+    print_sudoku(modified_sudoku)
+    print(action_log)
+
+'''
+getNotes()
+obvious()
+debug()
+'''
+
+getNotes()
+print_notes()
+print()
+while(True):
+    if(last_remaining_cell()): 
+        debug()
+        time.sleep(6)
+        continue
+
+    if(obvious()):
+        debug()
+        time.sleep(6)
+        continue
+
+    break
+print('End')
+debug()
